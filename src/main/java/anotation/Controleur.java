7713e23a5a5a4aea5c1a4eea1b7d9ff2e4014681
mahatoky <andrian.mahatoky@gmail.com>
2026-06-19 09:43:48 +0300

    creation du controleur

--- src/main/java/anotation/Controleur.java
+++ src/main/java/anotation/Controleur.java
@@ -1,5 +1,12 @@
 package anotation;
 
-public class Controleur {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Controleur {
     
 }
